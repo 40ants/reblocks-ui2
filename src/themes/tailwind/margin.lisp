0dd341586c5d24afcd00dfb0c3a2cbc1f2f6a872
Alexander Artemenko <svetlyak.40wt@gmail.com>
2024-01-14 19:33:40 +0000

    More styles + column now a function instead of a macro. Also, every widget now has a margin, width and height.

---
+++ src/themes/tailwind/margin.lisp
@@ -0,0 +1,56 @@
+(uiop:define-package #:reblocks-ui2/themes/tailwind/margin
+  (:use #:cl)
+  (:import-from #:reblocks-ui2/themes/styling
+                #:css-classes)
+  (:import-from #:reblocks-ui2/themes/tailwind
+                #:core-margin-sizes
+                #:tailwind-theme)
+  (:import-from #:alexandria
+                #:assoc-value)
+  (:import-from #:serapeum
+                #:fmt)
+  (:import-from #:reblocks-ui2/utils/margin
+                #:margin))
+(in-package #:reblocks-ui2/themes/tailwind/margin)
+
+
+(defmethod css-classes ((theme tailwind-theme) (margin margin) &key)
+  (let* ((sizes (core-margin-sizes theme))
+         (left (reblocks-ui2/utils/margin:margin-left margin))
+         (right (reblocks-ui2/utils/margin:margin-right margin))
+         (top (reblocks-ui2/utils/margin:margin-top margin))
+         (bottom (reblocks-ui2/utils/margin:margin-bottom margin))
+         (horizontal (when (eql left right)
+                       left))
+         (vertical (when (eql top bottom)
+                     top))
+         (total (when (eql horizontal vertical)
+                  horizontal)))
+    (uiop:while-collecting (collect)
+      (flet ((collect-value (margin-type value)
+               (collect
+                   (fmt "~A-~A"
+                        margin-type
+                        (assoc-value sizes
+                                     value)))))
+        (cond
+          (total
+           (collect-value "m" total))
+          (t
+           (cond
+             (horizontal
+              (collect-value "mx" horizontal))
+             (t
+              (when left
+                (collect-value "ml" left))
+              (when right
+                (collect-value "mr" right))))
+           
+           (cond
+             (vertical
+              (collect-value "my" vertical))
+             (t
+              (when top
+                (collect-value "mt" top))
+              (when bottom
+                (collect-value "mb" bottom))))))))))

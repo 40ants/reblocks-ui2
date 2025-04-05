d681655bb550e0a25cba887d19e704ae2a847a76
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-04-05 09:45:48 +0000

    Added missing file.

---
+++ src/themes/base.lisp
@@ -0,0 +1,9 @@
+(uiop:define-package #:reblocks-ui2/themes/base
+  (:use #:cl)
+  (:export #:base-theme))
+(in-package #:reblocks-ui2/themes/base)
+
+
+(defclass base-theme ()
+  ()
+  (:documentation "A base theme class for all Reblocks-UI2 themes."))
